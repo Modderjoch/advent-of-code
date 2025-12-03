3e8071a02bc3c64644ffbcc4639e5706c63a93b9
Oebe Rademaker <oeberademaker@desktop-kgkct78.home>
2025-12-03 20:37:58 +0100

    Cleaned the code slightly, still no fix for 3.2

--- 2025/Day 3/day3.py
+++ 2025/Day 3/day3.py
@@ -15,22 +15,50 @@ with open (file_path, 'r') as file:
     for i, bank in enumerate(banks):
         banks[i] = bank.replace("\n", "")
 
-    print(banks)
+    puzzle = input("Enter the solution you want to see.\n\n 1 or 2 \n\n")
 
-for i, battery in enumerate(banks):
+for i, bank in enumerate(banks):
 
-    batteries = [battery[i:i+1] for i in range(0, len(battery), 1)]
+    batteries = [bank[i:i+1] for i in range(0, len(bank), 1)]
 
-    largest_battery = max(batteries[:-1])
+    if(puzzle == "1"):
+        largest_battery = max(batteries[:-1])
 
-    largest_battery_index = batteries.index(largest_battery) + 1
+        largest_battery_index = batteries.index(largest_battery) + 1
 
-    new_batteries = batteries[largest_battery_index:]
+        new_batteries = batteries[largest_battery_index:]
 
-    second_largest_battery = max(new_batteries)
+        second_largest_battery = max(new_batteries)
 
-    largest_possible_joltage = int(str(largest_battery) + str(second_largest_battery))
+        largest_possible_joltage = int(str(largest_battery) + str(second_largest_battery))
 
-    total_output_joltage += largest_possible_joltage
+        total_output_joltage += largest_possible_joltage
+        
+    elif(puzzle == "2"):
+
+        largest_battery = max(batteries[:-11])
+
+        largest_battery_index = batteries.index(largest_battery) + 1
+
+        new_batteries = batteries[largest_battery_index:]
+
+        batteries_to_remove = len(new_batteries) - 11
+
+        for b in range(0, batteries_to_remove, 1):
+            lowest_battery = min(new_batteries)
+
+            lowest_battery_index = new_batteries.index(lowest_battery)
+
+            new_batteries.pop(lowest_battery_index)
+
+        largest_possible_joltage = largest_battery + "".join(new_batteries)
+
+        largest_joltage_int = int(largest_possible_joltage)
+
+        total_output_joltage += largest_joltage_int
 
 print("The total output joltage is " + str(total_output_joltage))
+
+# 169666831865734 is too low
+# 
+# 1695089027034985 is too high
